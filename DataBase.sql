c2151c0fc5a02333cf8736d81dbf0318f3942148
somazzilli <sofia.mazzilli@correo.ucu.edu.uy>
2026-06-04 12:48:25 -0300

    tablas terminadas  (revisar insc)

--- DataBase.sql
+++ DataBase.sql
@@ -4,14 +4,14 @@ USE entregablebd1;
 
 CREATE TABLE estudiantes
 (
-    #idEstudiante int auto_increment ???
+    id_estudiante int auto_increment,
     docuemnto   VARCHAR(50) NOT NULL UNIQUE, #unico por cada estudiante
     nombre      VARCHAR(50) NOT NULL,
     apellido    VARCHAR(50) NOT NULL,
     correo      VARCHAR(50) NOT NULL UNIQUE, #unico por cada estudiante
     carrera     VARCHAR(50) NOT NULL,
     facultad    VARCHAR(50) NOT NULL,
-    PRIMARY KEY (docuemnto)
+    PRIMARY KEY (id_estudiante)
 );
 
 CREATE TABLE disiplinas
@@ -23,19 +23,21 @@ CREATE TABLE disiplinas
 
 CREATE TABLE actividadesDeportivas
 (
-    id_actividad     INT AUTO_INCREMENT,
+    id_actividad    INT AUTO_INCREMENT,
     nombre          VARCHAR(50) NOT NULL,
     id_disiplina    INT NOT NULL,
     id_espacio      VARCHAR(50) NOT NULL,
     cupoMax         INT NOT NULL,
-    dia             ENUM('lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'), #unicos valores aceptados
+    dia_semana      ENUM('lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo'), #unicos valores aceptados
     fecha           DATE NOT NULL,
     horario         TIME NOT NULL,
     estado          ENUM('abierta', 'cerrada', 'finalizada', 'cancelada'), #unicos valores aceptados
 
     PRIMARY KEY (id_actividad),
     FOREIGN KEY (id_disiplina) REFERENCES disiplinas(id_disiplina),
-    FOREIGN KEY (id_espacio) REFERENCES espaciosDeportivos(id_espacio)
+    FOREIGN KEY (id_espacio) REFERENCES espaciosDeportivos(id_espacio),
+
+    CHECK (cupoMax > 0)
 );
 
 CREATE TABLE espaciosDeportivos
@@ -50,7 +52,12 @@ CREATE TABLE espaciosDeportivos
 
 CREATE TABLE inscripciones
 (
-    id_inscripcion      INT AUTO_INCREMENT
-    #id_estudiante o ci_estudiante foreign key
-    #activida_deportiva foreign key
+    id_inscripcion          INT AUTO_INCREMENT,
+    id_estudiante           INT, #evaluar unique
+    id_actividad_deportiva  INT,
+    fehca_inscrpcion        DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    estado                  ENUM ('confirmada','lista_espera','cancelada'),
+    PRIMARY KEY (id_inscripcion),
+    FOREIGN KEY (id_estudiante) REFERENCES estudiantes(id_estudiante),
+    FOREIGN KEY (id_actividad_deportiva) REFERENCES actividadesDeportivas(id_actividad)
 );
