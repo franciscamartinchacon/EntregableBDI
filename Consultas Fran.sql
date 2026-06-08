804269555f2d9d41543f44fea537517d39a8be73
franciscamartinchacon <franciscamarcha18@gmail.com>
2026-06-08 17:53:40 -0300

    correcciones y extras

--- Consultas Fran.sql
+++ Consultas Fran.sql
@@ -1,21 +1,19 @@
 USE entregablebd1;
 
 -- 5. Porcentaje de ocupación de cada actividad
-SELECT a.id_actividad, a.nombre, a.cupo_max, COUNT(i.id_inscripcion) AS confirmados
+SELECT a.id_actividad, a.nombre, a.cupo_max, COUNT(i.id_inscripcion) AS confirmados, ROUND((COUNT(i.id_inscripcion) / a.cupo_max) * 100, 2) AS porcentaje_ocupacion
 FROM actividadesDeportivas a
-LEFT JOIN inscripciones i
-    ON a.id_actividad = i.id_actividad_deportiva
+LEFT JOIN inscripciones i ON a.id_actividad = i.id_actividad_deportiva
     AND i.estado = 'confirmada'
 GROUP BY a.id_actividad, a.nombre, a.cupo_max
-ORDER BY ROUND(((confirmados / a.cupo_max) * 100),2) DESC;    -- Para que muestre solo dos decimales
+ORDER BY porcentaje_ocupacion DESC;
 
 -- 6. Porcentaje de asistencia por actividad
-SELECT a.nombre, ((COUNT(i.id_inscripcion)/COUNT(asis.presente))*100) as porcentaje
+SELECT a.nombre, ROUND(AVG(asis.presente) * 100, 2) AS porcentaje
 FROM actividadesDeportivas a
 JOIN inscripciones i ON i.id_actividad_deportiva = a.id_actividad
 JOIN asistencias asis ON i.id_inscripcion = asis.id_inscripcion
-    AND asis.presente = TRUE
-    AND i.estado = 'confirmada'
+WHERE i.estado = 'confirmada'
 GROUP BY a.nombre
 ORDER BY porcentaje DESC;
 
@@ -37,6 +35,24 @@ JOIN actividadesDeportivas a ON i.id_actividad_deportiva = a.id_actividad
 WHERE i.estado = 'lista_espera'
 ORDER BY a.nombre, i.fecha_inscripcion;
 
--- 9. Actividades con más porcentaje que el promedio
-SELECT a.nombre
+-- 9. Actividades con mayor porcentaje de asistencia que el promedio
+SELECT a.id_actividad, a.nombre AS actividad, ROUND(AVG(asis.presente) * 100, 2) AS porcentaje_asistencia
 FROM actividadesDeportivas a
+JOIN inscripciones i ON a.id_actividad = i.id_actividad_deportiva
+JOIN asistencias asis ON i.id_inscripcion = asis.id_inscripcion
+WHERE i.estado = 'confirmada'
+GROUP BY a.id_actividad, a.nombre
+HAVING porcentaje_asistencia > (
+    SELECT AVG(porcentaje)
+    FROM (
+        SELECT AVG(asis2.presente) * 100 AS porcentaje
+        FROM actividadesDeportivas a2
+        JOIN inscripciones i2 ON a2.id_actividad = i2.id_actividad_deportiva
+        JOIN asistencias asis2 ON i2.id_inscripcion = asis2.id_inscripcion
+        WHERE i2.estado = 'confirmada'
+        GROUP BY a2.id_actividad
+    ) AS porcentajes_por_actividad
+)
+ORDER BY porcentaje_asistencia DESC;
+
+-- 10.
