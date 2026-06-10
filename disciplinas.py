8a9546d1cfec9d7a0cc170577ff6365840ced845
somazzilli <sofia.mazzilli@correo.ucu.edu.uy>
2026-06-09 22:24:05 -0300

    disciplinas y archivo de validacion

--- disciplinas.py
+++ disciplinas.py
@@ -1,24 +1,31 @@
-#ABM disciplinas
-#depliega otro menu [agregar, borrar, modificar, disciplinas]
+from conexionSQL import get_connection
+from validacion_datos import pedir_texto_obligatorio
+from validacion_datos import pedir_entero
+from main import menu
 
-def ABM_disciplinas():
+#Depliega otro menu [agregar, borrar, modificar, disciplinas]
+
+def menu_disciplinas():
     while True:
         print("\n--- ABM disciplinas ---")
-        print("1. Agregar disciplina")
+        print("1. Alta disciplina") #Agregar
         print("2. Borrar disciplina")
         print("3. Actualizar disciplina")
-        print("4. Volver al menú principal")
+        print("4. Listar disciplinas")
+        print("5. Volver al menú principal")
         print("0. Salir")
 
         opcion = input("Seleccione una opción: ")
 
         if opcion == "1":
-            agregar_disciplina()
+            alta_disciplina()
         elif opcion == "2":
             borrar_disciplina()
         elif opcion == "3":
             actualizar_disciplina()
         elif opcion == "4":
+            listar_disciplinas()
+        elif opcion == "5":
             menu()
         elif opcion == "0":
             print("Saliendo...")
@@ -27,7 +34,172 @@ def ABM_disciplinas():
             print("Opción no válida")
 
 
-def agregar_disciplina():
-    while True:
+
+def alta_disciplina():
+    print("\n--- Alta de disciplinas ---")
+
+    nombre = pedir_texto_obligatorio("Nombre: ")
+
+    conexion = None
+    cursor = None
+
+    try:
+        conexion = get_connection()
+        cursor = conexion.cursor()
+
+        sql = """
+               INSERT INTO disciplinas 
+               (nombre)
+               VALUES (%s);
+           """
+
+        valores = (nombre,)
+
+        cursor.execute(sql, valores)
+        conexion.commit()
+
+        print("Disciplina creada correctamente.")
+
+    except Exception as e: #muestra el error específico?
+        print("Error al crear disciplina:")
+        print(e)
+        print("Puede ser que el nombre ya existan.")
+
+    finally:
+        if cursor is not None:#evita el error si algo falló al abrirse al abrir el cursor.
+            cursor.close()
+        if conexion is not None:
+            conexion.close()
 
 
+def borrar_disciplina():
+    print("\n--- Eliminar disciplina ---")
+
+    listar_disciplinas()
+
+    id_disciplina = pedir_entero("\nIngrese el ID del estudiante a eliminar: ")
+
+    confirmacion = input("¿Seguro que desea eliminar esta disciplina? (s/n): ").lower()
+
+    if confirmacion != "s":
+        print("Operación cancelada.")
+        return
+
+    conexion = None
+    cursor = None
+
+    try:
+        conexion = get_connection()
+        cursor = conexion.cursor()
+
+        sql = """
+                DELETE FROM disciplinas
+                WHERE id_disciplina = %s;
+            """
+
+        cursor.execute(sql, (id_disciplina,))
+        conexion.commit()
+
+        if cursor.rowcount > 0:
+            print("Disciplina eliminada correctamente.")
+        else:
+            print("No se encontró una disciplina con ese ID.")
+
+    except Exception as e:
+        print("Error al eliminar disciplina:")
+        print(e)
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conexion is not None:
+            conexion.close()
+
+
+def actualizar_disciplina():
+    """
+    Primero muestra la lista de disciplinas para que el usuario pueda ver el ID.
+    Luego pide los nuevos datos y ejecuta un UPDATE.
+    """
+    print("\n--- Actulizar disciplina ---")
+
+    listar_disciplinas()
+
+    id_disciplina = pedir_entero("\nIngrese el ID de la disciplina a modificar: ")
+
+    print("\nIngrese los nuevos datos de la disciplina:")
+    nombre = pedir_texto_obligatorio("Nuevo nombre: ")
+
+    conexion = None
+    cursor = None
+
+    try:
+        conexion = get_connection()
+        cursor = conexion.cursor()
+
+        sql = """
+            UPDATE disciplinas
+            SET nombre = %s,
+            WHERE id_disciplina = %s;
+        """
+
+        valores = (
+            nombre,
+            id_disciplina
+        )
+
+        cursor.execute(sql, valores)
+        conexion.commit()
+
+        if cursor.rowcount > 0:
+            print("Disciplina modificada correctamente.")
+        else:
+            print("No se encontró una disciplina con ese ID.")
+
+    except Exception as e:
+        print("Error al modificar disciplina:")
+        print(e)
+        print("Puede ser que el nombre ya exista.")
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conexion is not None:
+            conexion.close()
+
+def listar_disciplinas():
+    print("\n--- Listado de disciplinas ---")
+
+    conexion = None
+    cursor = None
+
+    try:
+        conexion = get_connection()
+        cursor = conexion.cursor()
+
+        sql = """
+                SELECT 
+                    d.id_disciplina, d.nombre
+                FROM disciplinas d
+                ORDER BY d.id_disciplina, d.nombre;
+            """
+
+        cursor.execute(sql)
+        encontro = False
+        for disciplina in cursor:
+            encontro = False
+            print(f"ID: {disciplina[0]} | Nombre: {disciplina[1]}")
+
+        if not encontro:
+            print("No hay disciplinas cargadas")
+
+
+    except Exception as e:
+        print("Error al listar estudiantes:")
+        print(e)
+
+    finally:
+        if cursor is not None:
+            cursor.close()
+        if conexion is not None:
+            conexion.close()
--- estudiantes.py
+++ estudiantes.py
@@ -1,28 +1,6 @@
 from conexionSQL import get_connection
-
-# Funciones para validar
-
-def pedir_texto_obligatorio(mensaje):
-    # Pide un texto por consola y valida que no esté vacío.
-    while True:
-        valor = input(mensaje).strip()
-
-        if valor != "":
-            return valor
-
-        print("Error. Este campo no puede estar vacío.")
-
-
-def pedir_entero(mensaje):
-
-    while True:
-        valor = input(mensaje).strip()
-
-        if valor.isdigit():
-            return int(valor)
-
-        print("Error: debe ingresar un número válido.")
-
+from validacion_datos import pedir_texto_obligatorio
+from validacion_datos import pedir_entero
 
 # MENÚ DE ESTUDIANTES
 
